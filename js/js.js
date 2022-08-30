dc08ea98ca97673eff61c0c84881592c80fccfbb
Luisa Upegui <luisisupegui12@gmail.com>
2022-08-30 12:14:29 -0500

    ONclick Event

---
+++ js/js.js
@@ -0,0 +1,14 @@
+function aceptar() {
+var add= document.querySelector(".text");
+add.remove();
+
+}
+
+
+function alerta(){
+    alert("Ninja was liked")
+}
+
+function cambiar() {
+    button.innerText="Logout"
+}
